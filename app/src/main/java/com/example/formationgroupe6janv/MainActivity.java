557b46277918006e1041e071c27c76d2724d9168
Mounir Ben Romdhane <mounir.benromdhane@esprit.tn>
2024-01-31 16:41:14 +0100

    test

--- app/src/main/java/com/example/formationgroupe6janv/MainActivity.java
+++ app/src/main/java/com/example/formationgroupe6janv/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.formationgroupe6janv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText val1,val2;
+    private Button btnPlus,btnDiv;
+    private TextView res;
+
+    private String v1,v2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        val1 = findViewById(R.id.val1);
+        val2 = findViewById(R.id.val2);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnDiv = findViewById(R.id.btnDiv);
+        res = findViewById(R.id.res);
+
+        btnPlus.setOnClickListener(v -> {
+            v1 = val1.getText().toString();
+            v2 = val2.getText().toString();
+            res.setText(Integer.toString(Integer.parseInt(v1) + Integer.parseInt(v2)));
+        });
+
+        btnDiv.setOnClickListener(v -> {
+            v1 = val1.getText().toString();
+            v2 = val2.getText().toString();
+            res.setText(Integer.toString(Integer.parseInt(v1) / Integer.parseInt(v2)));
+        });
+
     }
 }
